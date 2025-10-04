9a12c5a17e2c657149c962371f248c502fdc5c65
Luís Gouveia <luisgouveia74@gmail.com>
2025-10-04 11:35:52 -0300

    Copiando arquivos frontend para raiz do projeto para github pages

---
+++ scripts.js
@@ -0,0 +1,24 @@
+document.getElementById('emailForm').addEventListener('submit', async (e)=>{
+    e.preventDefault()
+
+    const text = document.getElementById('textInput').value
+    const file = document.getElementById('fileInput').files[0];
+
+    const formData = new FormData();
+    if (text) formData.append("text", text);
+    if (file) formData.append("file", file);
+
+    const res = await fetch("http://localhost:8000/process-email", {
+        method: "POST",
+        body: formData
+    });
+    console.log("Essa é a resposta:",res);
+
+    const data = await res.json();
+    console.log("Esse é o data json: ", data);
+    document.getElementById("categoria").textContent = data.categoria || "Erro";
+    document.getElementById("resposta").textContent = data.resposta || "Não gerada";
+
+    document.getElementById("results").classList.remove("hidden");
+
+})
