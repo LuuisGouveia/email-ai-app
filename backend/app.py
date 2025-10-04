41170098246535958dc052bd5a24919944f83339
Luís Gouveia <luisgouveia74@gmail.com>
2025-10-04 12:04:51 -0300

    Correção de urls nas rotas e no cors do front e backend

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 
 origins = [
-    "https://luuisgouveia.github.io/email-ai-app/"
+    "https://luuisgouveia.github.io"
     "http://127.0.0.1:5500",
     "http://localhost:5500"
 ]
